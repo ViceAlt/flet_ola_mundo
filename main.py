ebd7faf60f43928191fd5a1e99038cdf4499c367
Vito Virito <vitor.landin@aluno.senai.br>
2026-02-11 14:24:53 -0300

    Update main
    Update main

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import flet as ft
 def main(page: ft.Page):
     page.title = 'Meu programa em flet'
     page.bgcolor = '#e010c8'
-
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment= ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text('Vito'),
         ft.ElevatedButton('Clique Aqui')
